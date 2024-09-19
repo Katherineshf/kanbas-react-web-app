5e2c7def6397f603f3f6c8612a4a32b9d6f756ee
Katherine fang <katherinefang@Kats-MacDonald.local>
2024-09-19 13:21:52 -0700

    add info

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -7,6 +7,10 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
+        <li>            
+          <a id="wd-github" href="https://github.com/Katherineshf/kanbas-react-web-app">GitHub
+          </a>
+        </li>
       </ul>
     );
 }  
